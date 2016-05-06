d94f89320c615f1b7c457b691f6a5ea527ab8c27
Hansigzandrinovka <hansskywalkerr@gmail.com>
2016-05-06 15:04:07 -0500

    Update gamelogic.js
    Commenting gameLogic to clear up confusions. Also added second ='s on line 55 to make while(is now true) become while(if true) 
    
    Alternate implementation:
    turnOrder var tracks whose turn it is, pushing a button when not your turn does nothing
    playTurns() - takes an action (stand or hit), and a player (user or AI), then upon completion checks if next player is AI, and if so, determines their action based on an AI method, and then calls ITSELF with that player and the action.

--- gamelogic.js
+++ gamelogic.js
@@ -1,4 +1,4 @@
-function blackjackGame(){
+function blackjackGame(){ //game gets initialized when you create a blackjack game
   this.players = this.initPlayers()
 
   this.initPlayers = function(name){
@@ -14,7 +14,7 @@ function blackjackGame(){
   this.deck = new Deck();
   this.pot = 0;
 
-  this.startGame = function(name){
+  this.startGame = function(name){ //called when player clicks start button - every time game , it asks player to keep playing, which calls startGame method
     this.resetGame();
     this.dealCards();
     this.getBets();
@@ -22,13 +22,13 @@ function blackjackGame(){
     this.resolveGame();
   }
 
-  this.resetGame = function(){
+  this.resetGame = function(){ //prepare game for next round - every time game ends, game is reset
     this.resetPlayers();
     this.pot = 0;
     this.refreshScreen(this.players, this.pot, "Let's begin.");
   }
 
-  this.dealCards = function(){
+  this.dealCards = function(){ //at start of game, give each player 2 cards
     for(var i=0; i<this.players.length; i++){
       this.players[i].addCards(this.deck.drawCards(2));
       this.refreshScreen(this.players);
@@ -37,11 +37,11 @@ function blackjackGame(){
 
   this.resetPlayers = function(){
     for(var i=0; i<this.players.length; i++){
-      this.players[i].reset();
+      this.players[i].reset();  //each player needs a reset method that empties out player hand and resets bet stat to 0
     }
   }
 
-  this.getBets = function(){
+  this.getBets = function(){ //polls all players for the amount they want to bet for this game, taking the money from the players
     var bet = 0;
     for(var i=0; i<this.players.length; i++){
       this.pot += this.players[i].getBet(); //player object should update its bankroll
@@ -52,12 +52,12 @@ function blackjackGame(){
   this.playTurns(){
     for(var i=0; i<this.players.length; i++){
       var move = [];
-      while(this.players[i].busted = false && this.players[i].myTurn = true){
-        move = this.players[i].getMove();
-        if(move == 'stand'){
+      while(this.players[i].busted == false && this.players[i].myTurn == true){
+        move = this.players[i].getMove(); //it is player's turn - will he hit or stand? - for AI, getMove() returns current AI action
+        if(move == 'stand'){ //player waits for round to end
           this.players[i].myTurn = false;
         }
-        else if(move == 'hit'){
+        else if(move == 'hit'){ //player gets another card
           this.players[i].addCards(this.deck.drawCards(1));
           var amount = this.players[i].getTotalAmount();
           if(amount > 21){
@@ -73,7 +73,7 @@ function blackjackGame(){
     }
   }
 
-  this.getWinners = function(){
+  this.getWinners = function(){ //checks who is not busted, and determines who won
     var winners = [];
     var max = 0;
     for(var i=0; i<this.players.length; i++){
@@ -99,7 +99,7 @@ function blackjackGame(){
     var winners = this.getWinners();
   }
 
-  this.refreshScreen = function(players, message = ""){
+  this.refreshScreen = function(players, message = ""){ //presumably clears the screen, then updates it with new information
     setTimeout(function(){},2000); //maybe?
     //Call's to Alex's functions here
   }
@@ -108,7 +108,7 @@ function blackjackGame(){
 function Deck(){
   this.active = this.initDeck();
 
-  this.initDeck = function(){
+  this.initDeck = function(){ //builds deck randomly out of 4 decks of 52 cards
     for(var i=0; i<4; i++){
       for(var j=1; j<=13; j++){
         this.active.push(new Card('spade',i));
@@ -126,7 +126,7 @@ function Deck(){
     return this.active.splice(0,num);
   }
 
-  this.returnCards = function(cards){
+  this.returnCards = function(cards){ //returns cards to discard pile
     for(var i=0; i<cards.length; i++){
       this.discard.push(cards[i]);
     }
